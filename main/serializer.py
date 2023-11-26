f79967c4b4bcaa6c7b4dea1a45761635e289b0a6
Persona99 <vadim.zhelnin@bk.ru>
2023-11-27 00:37:49 +0500

    add imports view, serializer

--- main/serializer.py
+++ main/serializer.py
@@ -8,3 +8,29 @@ class ShopUnitImport(serializers.ModelSerializer):
         model = ShopUnit
         fields = ['id', 'name', 'parentId', 'type', 'price', 'date']
     
+    def update(self, instance: ShopUnit, validated_data):
+        instance.clean_fields()
+        return super().update(instance, validated_data)
+
+    def validate(self, data):            
+        if data.get('type') == 'CATEGORY' and data.get('price'):
+            raise serializers.ValidationError("Not need price for category")
+        if data.get('type') == 'CATEGORY':
+            data.update({'price': None})
+        if data.get('type') == "OFFER" and not data.get('price'):
+            raise serializers.ValidationError("Need price for offer")
+        if data.get('type') == "OFFER" and ShopUnit.objects.filter(parentId = data.get('id')).first():
+            raise serializers.ValidationError("Offer cant has child")
+        return data
+
+    def validated_date(self, value):
+        try:
+            datetime.fromisoformat(value)
+        except:
+            raise serializers.ValidationError('Wrong date format') 
+        return value
+    
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError('Price is less than 0')
+        return value
--- main/views.py
+++ main/views.py
@@ -7,8 +7,11 @@ from .models import ShopUnit
 # Create your views here.
 class Import_api(APIView):
     def post(self, request):
+        if 'updateDate' not in request.data:
+            return Response(status=400)
         for item in request.data['items']:
             instance = ShopUnit.objects.filter(id=item['id']).first()
+            item['date'] = request.data['updateDate']
             unit = ShopUnitImport(instance, data=item)
             if not unit.is_valid():
                 return Response(unit.errors, status=400)
